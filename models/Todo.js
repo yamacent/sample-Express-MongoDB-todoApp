57243acd0079d981c1cd9d77ba86ffeee37f5529
Daisuke Yamamoto <yamacentworld@yahoo.co.jp>
2015-04-03 16:59:29 +0900

    Create Todo Model with mongoose

---
+++ models/Todo.js
@@ -0,0 +1,10 @@
+var mongoose = require('mongoose');
+
+var TodoSchema = new mongoose.Schema({
+    name: String,
+    completed: Boolean,
+    note: String,
+    updated_at: { type: Date, default: Date.now }
+});
+
+module.exports = mongoose.model('Todo', TodoSchema);
